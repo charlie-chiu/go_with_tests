6d1878e2ab9ffd86b84de294a1ab31669de0299a
charlie <marmot00@gmail.com>
2020-02-01 11:55:12 +0800

    add dependency_injection

---
+++ dependency_injection/greet_test.go
@@ -0,0 +1,23 @@
+package dependency_injection
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Charlie")
+
+	got := buffer.String()
+	want := "Hello, Charlie"
+
+	assertStringEqual(t, got, want)
+}
+
+func assertStringEqual(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
