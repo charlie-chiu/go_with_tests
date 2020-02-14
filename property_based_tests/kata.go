812b4908884a359ee71628aab251a5f137925678
charlie <marmot00@gmail.com>
2020-02-14 22:31:56 +0800

    crazy amazing refactor

--- property_based_tests/kata.go
+++ property_based_tests/kata.go
@@ -10,6 +10,14 @@ type RomanNumeral struct {
 }
 
 var allRomanNumeral = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
 	{10, "X"},
 	{9, "IX"},
 	{5, "V"},
@@ -21,29 +29,9 @@ func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 
 	for _, romanNumeral := range allRomanNumeral {
-		for arabic > romanNumeral {
-
-		}
-	}
-
-	for arabic > 0 {
-
-		switch {
-		case arabic > 9:
-			result.WriteString("X")
-			arabic -= 10
-		case arabic > 8:
-			result.WriteString("IX")
-			arabic -= 9
-		case arabic > 4:
-			result.WriteString("V")
-			arabic -= 5
-		case arabic > 3:
-			result.WriteString("IV")
-			arabic -= 4
-		default:
-			result.WriteString("I")
-			arabic--
+		for arabic >= romanNumeral.Value {
+			result.WriteString(romanNumeral.Symbol)
+			arabic -= romanNumeral.Value
 		}
 	}
 
--- property_based_tests/kata_test.go
+++ property_based_tests/kata_test.go
@@ -1,29 +1,46 @@
 package property_based_tests
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRomanNumerals(t *testing.T) {
 	cases := []struct {
-		Name   string
 		Arabic int
 		Want   string
 	}{
-		{"1 gets converted to I", 1, "I"},
-		{"2 gets converted to II", 2, "II"},
-		{"3 gets converted to III", 3, "III"},
-		{"4 gets converted to IV", 4, "IV"},
-		{"5 gets converted to V", 5, "V"},
-		{"8 gets converted to VIII", 8, "VIII"},
-		{"9 gets converted to IX", 9, "IX"},
-		{"10 gets converted to X", 10, "X"},
-		{"14 gets converted to XIV", 14, "XIV"},
-		{"18 gets converted to XVIII", 18, "XVIII"},
-		{"20 gets converted to XX", 20, "XX"},
-		{"29 gets converted to XXIX", 29, "XXIX"},
+		{1, "I"},
+		{2, "II"},
+		{3, "III"},
+		{4, "IV"},
+		{5, "V"},
+		{8, "VIII"},
+		{9, "IX"},
+		{10, "X"},
+		{14, "XIV"},
+		{18, "XVIII"},
+		{20, "XX"},
+		{29, "XXIX"},
+		{39, "XXXIX"},
+		{40, "XL"},
+		{50, "L"},
+		{60, "LX"},
+		{68, "LXVIII"},
+		{100, "C"},
+		{90, "XC"},
+		{125, "CXXV"},
+		{500, "D"},
+		{400, "CD"},
+		{888, "DCCCLXXXVIII"},
+		{900, "CM"},
+		{1000, "M"},
+		{1984, "MCMLXXXIV"},
+		{1006, "MVI"},
 	}
 
 	for _, testCase := range cases {
-		t.Run(testCase.Name, func(t *testing.T) {
+		t.Run(fmt.Sprintf("%d gets converted to %q", testCase.Arabic, testCase.Want), func(t *testing.T) {
 			got := ConvertToRoman(testCase.Arabic)
 			want := testCase.Want
 
