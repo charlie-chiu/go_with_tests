7b3e4f99014e5bc3da198ef10d12ecec0d6580f5
charlie chiu <marmot00@gmail.com>
2020-02-24 10:50:25 +0800

    move application to root directory, sync code with JSON, routing and embedding.

--- main.go
+++ main.go
@@ -1,30 +1,14 @@
 package main
 
 import (
-	"github.com/charlie-chiu/go_with_test/application"
 	"log"
 	"net/http"
 )
 
-type InMemoryPlayerStore struct {
-	store map[string]int
-}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return i.store[name]
-}
-
-func (i *InMemoryPlayerStore) RecordWin(name string) {
-	i.store[name]++
-}
-
 func main() {
-	handler := &application.PlayerServer{
-		&InMemoryPlayerStore{store: map[string]int{}},
-	}
+	server := &PlayerServer{NewInMemoryPlayerStore()}
 
-	if err := http.ListenAndServe(":8080", handler); err != nil {
-		log.Fatalf("could not listen on port 8080, %v", err)
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
 	}
-
 }
--- server.go
+++ server.go
@@ -1,9 +1,8 @@
-package application
+package main
 
 import (
 	"fmt"
 	"net/http"
-	"strings"
 )
 
 type PlayerStore interface {
@@ -12,31 +11,31 @@ type PlayerStore interface {
 }
 
 type PlayerServer struct {
-	Store PlayerStore
+	store PlayerStore
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	player := r.URL.Path[len("/players/"):]
 
 	switch r.Method {
 	case http.MethodPost:
 		p.processWin(w, player)
 	case http.MethodGet:
-		p.processScore(w, player)
+		p.showScore(w, player)
 	}
 }
 
-func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
-	p.Store.RecordWin(player)
-	w.WriteHeader(http.StatusAccepted)
-}
-
-func (p *PlayerServer) processScore(w http.ResponseWriter, player string) {
-	score := p.Store.GetPlayerScore(player)
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
+	score := p.store.GetPlayerScore(player)
 
 	if score == 0 {
 		w.WriteHeader(http.StatusNotFound)
 	}
 
 	fmt.Fprint(w, score)
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
+	p.store.RecordWin(player)
+	w.WriteHeader(http.StatusAccepted)
 }
