471d4d379031ea65a673c497a05ba6c815ecec46
charlie chiu <marmot00@gmail.com>
2020-02-26 16:12:02 +0800

    completed this chapter

--- server_test.go
+++ server_test.go
@@ -91,6 +91,7 @@ func TestPOST(t *testing.T) {
 }
 
 func TestRecordingWinsAndRetrievingThem(t *testing.T) {
+	// run integration test by using InMemoryPlayerStore instead of StubPlayerStore
 	store := InMemoryPlayerStore{store: map[string]int{}}
 	server := NewPlayerServer(&store)
 	player := "charlie"
@@ -99,11 +100,24 @@ func TestRecordingWinsAndRetrievingThem(t *testing.T) {
 	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
 	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
 
-	response := httptest.NewRecorder()
-	server.ServeHTTP(response, newGetScoreRequest(player))
-	assertStatus(t, response.Code, http.StatusOK)
+	t.Run("get score", func(t *testing.T) {
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, newGetScoreRequest(player))
+		assertStatus(t, response.Code, http.StatusOK)
+		assertResponseBody(t, response.Body.String(), "3")
+	})
 
-	assertResponseBody(t, response.Body.String(), "3")
+	t.Run("get league", func(t *testing.T) {
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, newLeagueRequest())
+		assertStatus(t, response.Code, http.StatusOK)
+		got := getLeagueFromResponse(t, response.Body)
+		want := []Player{
+			{"charlie", 3},
+		}
+		assertLeague(t, got, want)
+	})
+
 }
 
 func TestLeague(t *testing.T) {
