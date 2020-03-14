6383c1a87654dc6edfc2d480eae100e6e933f4e0
charlie <marmot00@gmail.com>
2020-03-14 13:51:19 +0800

    move texas holdem to separate file

--- game.go
+++ game.go
@@ -1,36 +1,6 @@
 package poker
 
-import "time"
-
 type Game interface {
 	Start(numberOfPlayers int)
 	Finish(winner string)
-}
-
-type TexasHoldem struct {
-	alerter BlindAlerter
-	store   PlayerStore
-}
-
-// constructor of TexasHoldem
-func NewTexasHoldem(a BlindAlerter, s PlayerStore) *TexasHoldem {
-	return &TexasHoldem{
-		alerter: a,
-		store:   s,
-	}
-}
-
-func (t *TexasHoldem) Start(numberOfPlayers int) {
-	blindIncrement := time.Duration(5+numberOfPlayers) * time.Minute
-
-	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
-	blindTime := 0 * time.Second
-	for _, blind := range blinds {
-		t.alerter.ScheduleAlertAt(blindTime, blind)
-		blindTime = blindTime + blindIncrement
-	}
-}
-
-func (t *TexasHoldem) Finish(winner string) {
-	t.store.RecordWin(winner)
 }
---
+++ texas_holdem.go
@@ -0,0 +1,32 @@
+package poker
+
+import "time"
+
+type TexasHoldem struct {
+	alerter BlindAlerter
+	store   PlayerStore
+}
+
+// constructor of TexasHoldem
+func NewTexasHoldem(a BlindAlerter, s PlayerStore) *TexasHoldem {
+	return &TexasHoldem{
+		alerter: a,
+		store:   s,
+	}
+}
+
+func (t *TexasHoldem) Start(numberOfPlayers int) {
+	//blindIncrement := time.Duration(5+numberOfPlayers) * time.Minute
+	blindIncrement := 5 * time.Second
+
+	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
+	blindTime := 0 * time.Second
+	for _, blind := range blinds {
+		t.alerter.ScheduleAlertAt(blindTime, blind)
+		blindTime = blindTime + blindIncrement
+	}
+}
+
+func (t *TexasHoldem) Finish(winner string) {
+	t.store.RecordWin(winner)
+}
