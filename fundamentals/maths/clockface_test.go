a9a474ff825c6c83b09ad79a2213bb0fe84b221e
charlie <marmot00@gmail.com>
2020-04-03 22:51:53 +0800

    part2 : align with book

--- fundamentals/maths/clockface_test.go
+++ fundamentals/maths/clockface_test.go
@@ -1,29 +1,64 @@
-package clockface_test
+package clockface
 
 import (
-	clockface "github.com/charlie-chiu/go_with_test/fundamentals/maths"
+	"math"
 	"testing"
 	"time"
 )
 
-func TestSecondHandAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+func TestSecondHandInRadians(t *testing.T) {
+	cases := []struct {
+		t     time.Time
+		angle float64
+	}{
+		{simpleTime(0, 0, 30), math.Pi},
+		{simpleTime(0, 0, 0), 0},
+		{simpleTime(0, 0, 45), (math.Pi / 2) * 3},
+		{simpleTime(0, 0, 7), (math.Pi / 30) * 7},
+	}
 
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
-
-	if got != want {
-		t.Errorf("got %v, want %v", got, want)
+	for _, c := range cases {
+		t.Run(testName(c.t), func(t *testing.T) {
+			got := secondsInRadians(c.t)
+			if got != c.angle {
+				t.Errorf("Wanted %v radians, got %v", c.angle, got)
+			}
+		})
 	}
 }
 
-func TestSecondHandAt30Seconds(t *testing.T) {
-	tm := time.Date(1337, time.September, 1, 0, 0, 30, 0, time.UTC)
-
-	want := clockface.Point{X: 150, Y: 150 + 90}
-	got := clockface.SecondHand(tm)
-
-	if got != want {
-		t.Errorf("got %v, want %v", got, want)
+func TestSecondHandVector(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		point Point
+	}{
+		{simpleTime(0, 0, 30), Point{0, -1}},
+		{simpleTime(0, 0, 45), Point{-1, 0}},
 	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := secondHandPoint(c.time)
+			if !roughlyEqualPoint(c.point, got) {
+				t.Errorf("wanted %v point got %v", c.point, got)
+			}
+		})
+	}
+}
+
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(312, time.January, 1, hours, minutes, seconds, 0, time.UTC)
+}
+
+func testName(time time.Time) string {
+	return time.Format("15:04:05")
+}
+
+func roughlyEqualFloat64(a, b float64) bool {
+	const equalityThreshold = 1e-7
+	return math.Abs(a-b) < equalityThreshold
+}
+
+func roughlyEqualPoint(a, b Point) bool {
+	return roughlyEqualFloat64(a.X, b.X) && roughlyEqualFloat64(a.Y, b.Y)
 }
