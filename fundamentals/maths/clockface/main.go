585a2a2d2524daaee41f034bd95b0c3938733947
charlie <marmot00@gmail.com>
2020-04-04 09:47:25 +0800

    part2 : with some progress

--- fundamentals/maths/clockface/main.go
+++ fundamentals/maths/clockface/main.go
@@ -1,1 +1,12 @@
-package clockface
+package main
+
+import (
+	clockface "github.com/charlie-chiu/go_with_test/fundamentals/maths"
+	"os"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+	clockface.SVGWriter(os.Stdout, t)
+}
--- fundamentals/maths/clockface_acceptance_test.go
+++ fundamentals/maths/clockface_acceptance_test.go
@@ -1,10 +1,33 @@
 package clockface_test
 
 import (
+	"bytes"
+	"encoding/xml"
 	clockface "github.com/charlie-chiu/go_with_test/fundamentals/maths"
 	"testing"
 	"time"
 )
+
+func TestSVGWriterAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	b := bytes.Buffer{}
+
+	clockface.SVGWriter(&b, tm)
+	svg := clockface.Svg{}
+	xml.Unmarshal(b.Bytes(), &svg)
+
+	x2 := "150.000"
+	y2 := "60.000"
+
+	for _, line := range svg.Line {
+		if line.X2 == x2 && line.Y2 == y2 {
+			return
+		}
+	}
+
+	t.Errorf("expected to find the second hand with x2 of %+v and y2 of %+v, in the SVG output %v", x2, y2, b.String())
+}
 
 func TestSecondHandAtMidnight(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
